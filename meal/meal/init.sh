93bf1c9b34469ba9f8ac61057e29c74a043b8c23
ORLANDO-VECTOR\orlando <isc.orlando.valencia@gmail.com>
2024-08-17 01:23:23 -0600

    Dockerizing Meal

---
+++ meal/meal/init.sh
@@ -0,0 +1,20 @@
+# Pull mongodb
+docker pull mongodb/mongodb-community-server
+
+# Run mongodb as a container
+docker run --name mongocontainer -d mongodb/mongodb-community-server:latest
+
+# docker network create ${network_name}
+docker network create networkmongo
+
+# docker network connect ${network_name} ${container_name}
+docker network connect networkmongo mongocontainer
+
+# build application
+mvn clean package compile install -DskipTests
+
+# create the image
+docker build -t meal .
+
+# run application container using mongodb connection
+docker run -d -p 6666:6666 --name mealcontainer --net networkmongo -e SERVER_PORT=6666 -e MONGODB_HOST=mongocontainer -e MONGODB_PORT=27017 -e MONGODB_DBNAME=meal-org -e MONGODB_USER=admin -e MONGODB_PASSWORD=admin meal
