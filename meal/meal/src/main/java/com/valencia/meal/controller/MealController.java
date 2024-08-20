2e603dcd917fbdcf504a48315d27b1121cb73670
ORLANDO-VECTOR\orlando <isc.orlando.valencia@gmail.com>
2024-08-20 15:42:37 -0600

    Adding Logs To Meal

--- meal/meal/src/main/java/com/valencia/meal/controller/MealController.java
+++ meal/meal/src/main/java/com/valencia/meal/controller/MealController.java
@@ -4,6 +4,8 @@ import com.valencia.meal.entity.Meal;
 import com.valencia.meal.repository.MealRepository;
 import com.valencia.meal.service.SequenceGeneratorService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,8 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class MealController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MealController.class);
+
     @Autowired
     private MealRepository mealRepository;
 
@@ -26,11 +30,13 @@ public class MealController {
 
     @GetMapping("/meals")
     public List<Meal> getAllMeals() {
+        LOG.info("find All Meals");
         return mealRepository.findAll();
     }
 
     @GetMapping("/meal/{id}")
     public ResponseEntity<Optional<Meal>> getUserById(@PathVariable(value = "id") Long mealId) throws Exception {
+        LOG.info("get meal by Id : " + mealId);
         Optional<Meal> meal = Optional.ofNullable(mealRepository.findById(mealId)
                 .orElseThrow(() -> new Exception("Meal not found for this id :: " + mealId)));
         return ResponseEntity.ok().body(meal);
@@ -57,16 +63,19 @@ public class MealController {
         meal.setPreparation(fromMeal.getPreparation());
         meal.setIngredients(fromMeal.getIngredients());
 
+        LOG.info("Saving a meal : " + meal.getId());
         return ResponseEntity.ok(mealRepository.save(meal));
     }
 
     @PutMapping("/meal/{id}")
     public ResponseEntity<Meal> updateMeal(@PathVariable(value = "id") Long mealId, @Valid @RequestBody Meal fromMeal) throws Exception {
+        LOG.info("Looking for meal : " + mealId);
         Optional<Meal> meal = Optional.ofNullable(mealRepository.findById(mealId)
                 .orElseThrow(() -> new Exception("Meal not found for this id :: " + mealId)));
 
         Meal thisMeal = new Meal();
         if (meal.isPresent()) {
+            LOG.info("Meal is present? : " + (meal.isPresent() ? "YES" : "NO"));
             thisMeal = meal.get();
         }
 
@@ -77,17 +86,20 @@ public class MealController {
         thisMeal.setIngredients(fromMeal.getIngredients());
 
         final Meal updatedMeal = mealRepository.save(thisMeal);
+        LOG.info("Meal updated");
         return ResponseEntity.ok(updatedMeal);
     }
 
     @DeleteMapping("/meal/{id}")
     public Map< String, Boolean > deleteMeal(@PathVariable(value = "id") Long mealId) throws Exception {
+        LOG.info("Looking for meal : " + mealId);
         Optional<Meal> meal = Optional.ofNullable(mealRepository.findById(mealId)
                 .orElseThrow(() -> new Exception("Meal not found for this id :: " + mealId)));
 
         mealRepository.delete(meal.get());
         Map < String, Boolean > response = new HashMap< >();
         response.put("deleted", Boolean.TRUE);
+        LOG.info("Meal deleted : " + mealId);
         return response;
     }
 }
