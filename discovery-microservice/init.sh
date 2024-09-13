4fb22005aaab61246cb6c417039830490648f0bb
ORLANDO-VECTOR\orlando <isc.orlando.valencia@gmail.com>
2024-09-12 18:12:30 -0600

    Dockerize service Discovery

--- discovery-microservice/init.sh
+++ discovery-microservice/init.sh
@@ -1,5 +1,11 @@
+# build application
+mvn clean package compile install -DskipTests
+
 # create the image
 docker build -t discoverymicroservice .
 
 # run application container
 docker run --name discoveryservicecontainer -p 8761:8761 -d discoverymicroservice
+
+# docker network connect ${network_name} ${container_name}
+docker network connect networkmongo discoveryservicecontainer
