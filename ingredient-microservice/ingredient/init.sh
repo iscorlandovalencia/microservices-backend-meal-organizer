70279cdb580b45860f465095bc68ea1d04750290
ORLANDO-VECTOR\orlando <isc.orlando.valencia@gmail.com>
2025-10-28 13:00:01 -0600

    Adding Configuration fixes for Ingredient-Microservice

--- ingredient-microservice/ingredient/init.sh
+++ ingredient-microservice/ingredient/init.sh
@@ -1,11 +1,11 @@
 # Pull mongodb
 docker pull mongodb/mongodb-community-server
 
-# Run mongodb as a container
-docker run -d -p 27017:27017 --name mongo-container -d mongodb/mongodb-community-server:latest
-
 # docker network create ${network_name}
 docker network create network-mongo
+
+# Run mongodb as a container
+docker run -d --name mongo-container --net network-mongo -p 27017:27017 mongo:latest
 
 # docker network connect ${network_name} ${container_name}
 docker network connect network-mongo mongo-container
@@ -17,4 +17,11 @@ mvn clean package compile install -DskipTests
 docker build -t ingredient-microservice .
 
 # run application container using mongodb connection
-docker run --name ingredient-container --net networkmongo -e SERVER_PORT=5555 -e MONGODB_HOST=mongo-container -e MONGODB_PORT=27017 -e MONGODB_DBNAME=meal-org -e MONGODB_USER=admin -e MONGODB_PASSWORD=admin -d ingredient-microservice
+docker run --name ingredient-container --net networkmongo \
+  -e SERVER_PORT=5555 \
+  -e MONGODB_HOST=mongo-container \
+  -e MONGODB_PORT=27017 \
+  -e MONGODB_DBNAME=meal-organizer-data \
+  ingredient-microservice
+  #-e MONGODB_USER=admin \
+  #-e MONGODB_PASSWORD=admin \
